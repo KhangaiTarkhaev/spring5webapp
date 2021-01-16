ea61dd72bddde829d91c2d3240687fd8388046e6
Khangai Tarkhaev <hangaitarhaev95@gmail.com>
2021-01-16 20:40:12 +0800

    Added Publisher

--- src/main/java/khangai/springframefwork/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/khangai/springframefwork/spring5webapp/bootstrap/BootStrapData.java
@@ -2,8 +2,10 @@ package khangai.springframefwork.spring5webapp.bootstrap;
 
 import khangai.springframefwork.spring5webapp.model.Author;
 import khangai.springframefwork.spring5webapp.model.Book;
+import khangai.springframefwork.spring5webapp.model.Publisher;
 import khangai.springframefwork.spring5webapp.repositories.AuthorRepository;
 import khangai.springframefwork.spring5webapp.repositories.BookRepository;
+import khangai.springframefwork.spring5webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,28 +14,47 @@ public class BootStrapData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        Publisher publisher = new Publisher();
+        publisher.setName("GEOTAR");
+        publisherRepository.save(publisher);
+
         Author eric = new Author("Eric", "Evans");
         Book springHeadFirst = new Book("Spring Head First", "123123");
         eric.getBooks().add(springHeadFirst);
         springHeadFirst.getAuthors().add(eric);
+        springHeadFirst.setPublisher(publisher);
+        publisher.getBooks().add(springHeadFirst);
+
         authorRepository.save(eric);
         bookRepository.save(springHeadFirst);
+        publisherRepository.save(publisher);
+
         Author rod = new Author("Rod", "Johnson");
         Book noEJB = new Book("NoEJB", "2424245");
         rod.getBooks().add(noEJB);
         noEJB.getAuthors().add(rod);
+        noEJB.setPublisher(publisher);
+        publisher.getBooks().add(noEJB);
+
         authorRepository.save(rod);
         bookRepository.save(noEJB);
+        publisherRepository.save(publisher);
+
+
 
         System.out.println("Started a Bootstrap");
         System.out.println("Number of Books:" + bookRepository.count());
+        System.out.println("Number of Publishers: "  + publisherRepository.count());
+        System.out.println("Number of Books by Publisher: " + publisher.getBooks().size());
     }
 }
---
+++ src/main/java/khangai/springframefwork/spring5webapp/repositories/PublisherRepository.java
@@ -0,0 +1,8 @@
+package khangai.springframefwork.spring5webapp.repositories;
+
+import khangai.springframefwork.spring5webapp.model.Publisher;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PublisherRepository extends CrudRepository<Publisher, Long> {
+
+}
