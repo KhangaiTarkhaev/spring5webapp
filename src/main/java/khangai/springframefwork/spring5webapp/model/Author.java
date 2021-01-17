823d0a0ab4c348d82537b083459b56359da080c7
Khangai Tarkhaev <hangaitarhaev95@gmail.com>
2021-01-17 20:49:20 +0800

    Added MVC + Thymeleaf

--- src/main/java/khangai/springframefwork/spring5webapp/model/Author.java
+++ src/main/java/khangai/springframefwork/spring5webapp/model/Author.java
@@ -68,4 +68,13 @@ public class Author {
     public int hashCode() {
         return Objects.hash(id);
     }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
 }
